dc08c2daa540aeb355b153ff5242d7eecdd0b827
imranahamad9994 <imran.ahamad9994@gmail.com>
2026-03-30 14:11:26 +0530

    Cross browser Test Added

--- src/test/java/com/imran/automation/base/BaseTest.java
+++ src/test/java/com/imran/automation/base/BaseTest.java
@@ -3,14 +3,21 @@ package com.imran.automation.base;
 import com.imran.automation.factory.DriverFactory;
 import com.imran.automation.utils.ConfigReader;
 import org.openqa.selenium.WebDriver;
+import org.testng.annotations.Optional;
+import org.testng.annotations.Parameters;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 
 public abstract class BaseTest {
 
     @BeforeMethod(alwaysRun = true)
-    public void setUp() {
-        DriverFactory.initializeDriver(ConfigReader.get("browser"));
+    @Parameters("browser")
+    public void setUp(@Optional String browser) {
+        String browserName = (browser == null || browser.isBlank())
+                ? ConfigReader.get("browser")
+                : browser;
+
+        DriverFactory.initializeDriver(browserName);
         getDriver().get(ConfigReader.get("baseUrl"));
     }
 
